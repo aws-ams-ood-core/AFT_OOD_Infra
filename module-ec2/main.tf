e3f506f3d8ded2632d449d735cbda3d3b97e7dc2
aws-ams-ood-core <174574987+aws-ams-ood-core@users.noreply.github.com>
2024-07-11 05:48:33 +0000

    fixed ami data

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -11,14 +11,32 @@ data "aws_subnet" "selected" {
   }
 }
 
+data "aws_ami" "windows-ami" {
+  filter {
+    name = "name"
+    values = ["Windows_Server-2019-English-Full-Base2021*"]
+  }
+  filter {
+    name = "platform"
+    values = ["Windows"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  most_recent = true
+  owners = ["amazon"]
+}
+
 resource "aws_security_group" "allow_http" {
   name        = "allow-http"
   description = "Allow http inbound traffic"
   vpc_id      = data.aws_vpc.selected.id
 }
+
 # Create aws ec2 instance for the app server
 resource "aws_instance" "AFT-TestInstance1r" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.windows-ami.id
   instance_type          = var.instance
   vpc_security_group_ids = [aws_security_group.allow_http.id]
   subnet_id              = data.aws_subnet.selected.id
